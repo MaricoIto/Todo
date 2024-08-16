561056b7e0ec2a477151640f754cc74b6a4bb981
Maricos <itomarico15@gmail.com>
2024-08-16 19:18:57 +0900

    2-7

--- src/app/Models/Todo.php
+++ src/app/Models/Todo.php
@@ -15,4 +15,26 @@ class Todo extends Model
     {
         return $this->belongsTo(Category::class);
     }
+
+    public function scopeCategorySearch($query, $category_id)
+    {
+        if (!empty($category_id)) {
+            $query->where('category_id', $category_id);
+        }
+    }
+
+    public function scopeKeywordSearch($query, $keyword)
+    {
+        if (!empty($keyword)) {
+            $query->where('content', 'like', '%' . $keyword . '%');
+        }
+    }
+
+    public function search(Request $request)
+    {
+        $todos = Todo::with('category')->CategorySearch($request->category_id)->KeywordSearch($request->keyword)->get();
+        $categories = Category::all();
+
+        return view('index', compact('todos', 'categories'));
+    }
 }
